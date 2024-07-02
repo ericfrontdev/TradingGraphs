470e19ba530d60f94d87be1cf4f1f41c93f4cb2c
Eric Ouellette <ericfrontdev@outlook.com>
2024-07-02 11:40:55 -0400

    config

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,5 +1,4 @@
 // https://github.com/vitejs/vite/discussions/3448
-import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import jsconfigPaths from 'vite-jsconfig-paths';
@@ -7,32 +6,6 @@ import jsconfigPaths from 'vite-jsconfig-paths';
 // ----------------------------------------------------------------------
 
 export default defineConfig({
+  base: '/trading/graphs/',
   plugins: [react(), jsconfigPaths()],
-  define: {
-    global: 'window'
-  },
-  resolve: {
-    alias: [
-      {
-        find: /^~(.+)/,
-        replacement: path.join(process.cwd(), 'node_modules/$1')
-      },
-      {
-        find: /^src(.+)/,
-        replacement: path.join(process.cwd(), 'src/$1')
-      }
-    ]
-  },
-  server: {
-    // this ensures that the browser opens upon server start
-    open: true,
-    // this sets a default port to 3000
-    port: 3000
-  },
-  preview: {
-    // this ensures that the browser opens upon preview start
-    open: true,
-    // this sets a default port to 3000
-    port: 3000
-  }
 });
